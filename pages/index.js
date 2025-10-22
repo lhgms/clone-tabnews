9ba834c0feb16787de4dd9427efc3367408b7fc1
lhgms <93497066+lhgms@users.noreply.github.com>
2025-10-22 00:33:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return<h1>Mensagem de teste</h1>
+  return <h1>Mensagem de teste</h1>;
 }
 
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("TESTE2");
+}
 export default Home;
